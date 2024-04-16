1ccaf67b57f0338cbac4c610f812b105b016f93c
Steve <steve.hoareau1@gmail.com>
2024-04-16 21:23:14 +0400

    [style] remove unused commentary

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   build: {
     rollupOptions: {
-      // external: ["svelte"],
       input: {
         main: resolve(__dirname, "index.html"),
         bac: resolve(__dirname, "bac/index.html"),
